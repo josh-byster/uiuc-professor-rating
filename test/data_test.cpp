61a4d43498a43c6ba4e4fffd8d6dd7f714b794d2
Josh Byster <joshb1050@gmail.com>
2018-04-30 21:39:19 -0500

    Finished test formatting/commenting

--- test/data_test.cpp
+++ test/data_test.cpp
@@ -7,18 +7,41 @@
 #include <cmath>
 
 DataFrame testFrame;
+
 void setup(){
     testFrame = DataFrame(FILE_PATH);
 }
+
 bool isWithinTolerance(double a, double b){
     return std::abs(a-b) < EPSILON;
 }
+
+//---------------------------SECTION--------------------------
+//Spot testing
 TEST_CASE("Simple test of getting GPA"){
     setup();
     SemesterClass s(2012,"Fall","BIOE",504);
     REQUIRE(isWithinTolerance(testFrame.getSemesterClassGPA(s), 3.64));
 }
 
+TEST_CASE("Spot check for course number"){
+    SemesterClass test(2012,"Fall","BTW",250);
+    REQUIRE(20 == testFrame.getSemesterClassMap().at(test).size());
+}
+
+TEST_CASE("Spot check GPA by instructor"){
+   SemesterClass test(2012,"Fall","BTW",250);
+   std::string exampleNames[3] = {"Wetter, Timothy S","Kelly, Daniel","Vredenburg, Jason"};
+   double correspondingGPA[3] = {3.565,3.462,3.5491};
+   for(int i = 0; i < 3; i++){
+       double computedGPA = testFrame.getGPAByCourseVector(testFrame.getSemesterClassMapByInstructor(exampleNames[i]).at(test)); 
+       REQUIRE(isWithinTolerance(computedGPA,correspondingGPA[i]));
+   }
+   
+}
+
+//---------------------------SECTION--------------------------
+//Testing for consistency in dataframe setup
 TEST_CASE("Test that every professor has some valid name"){
     for(const std::string& name : testFrame.getAllInstructorNames()){
         REQUIRE(name.find(", ") != std::string::npos);
@@ -45,22 +68,22 @@ TEST_CASE("Test that there are the same amount of courses in both maps"){
     REQUIRE(semesterClassMapSize == instructorMapSize);
 }
 
-TEST_CASE("Spot check for course number"){
-    SemesterClass test(2012,"Fall","BTW",250);
-    REQUIRE(20 == testFrame.getSemesterClassMap().at(test).size());
+TEST_CASE("Check that instructor course map is correct"){
+    // Note that even though this is a triple for loop, it's still O(n) on
+    // the number of lines in the CSV file
+    for(const std::string& name : testFrame.getAllInstructorNames()){
+        for(const std::pair<SemesterClass,std::vector<Course>>& classPair 
+        : testFrame.getSemesterClassMapByInstructor(name)){
+            for(const Course& course : classPair.second){
+                REQUIRE(name == course.instructorName);
+                REQUIRE(classPair.first == course.semesterClass);
+            }
+        }
+    }
 }
 
-TEST_CASE("Spot check GPA by instructor"){
-   SemesterClass test(2012,"Fall","BTW",250);
-   std::string exampleNames[3] = {"Wetter, Timothy S","Kelly, Daniel","Vredenburg, Jason"};
-   double correspondingGPA[3] = {3.565,3.462,3.5491};
-   for(int i = 0; i < 3; i++){
-       double computedGPA = testFrame.getGPAByCourseVector(testFrame.getSemesterClassMapByInstructor(exampleNames[i]).at(test)); 
-       REQUIRE(isWithinTolerance(computedGPA,correspondingGPA[i]));
-   }
-   
-}
-
+//---------------------------SECTION--------------------------
+//Testing search functionality
 TEST_CASE("Check that the instructor search results are limited correctly"){
     for(int i = 0; i < 10; i++){
         std::vector<std::string> matches = testFrame.getProfessorMatchesByName("smith", i);
@@ -78,6 +101,8 @@ TEST_CASE("Check that the instructor search results are actually contain the str
     }
 }
 
+//---------------------------SECTION--------------------------
+//Testing rankings
 TEST_CASE("Check that for one instructor for a course, rank is correct"){
     SemesterClass discreteStructures(2017,"Fall","CS",173);
     std::pair<int,int> rank = testFrame.getInstructorRankForSemesterClass(discreteStructures,"Fleck, Margaret M");
@@ -105,20 +130,8 @@ TEST_CASE("Test that the ranks are correctly ordered"){
     }
 }
 
-TEST_CASE("Check that instructor course map is correct"){
-    // Note that even though this is a triple for loop, it's still O(n) on
-    // the number of lines in the CSV file
-    for(const std::string& name : testFrame.getAllInstructorNames()){
-        for(const std::pair<SemesterClass,std::vector<Course>>& classPair 
-        : testFrame.getSemesterClassMapByInstructor(name)){
-            for(const Course& course : classPair.second){
-                REQUIRE(name == course.instructorName);
-                REQUIRE(classPair.first == course.semesterClass);
-            }
-        }
-    }
-}
-
+//---------------------------SECTION--------------------------
+// Testing GPA excluding instructor
 TEST_CASE("Edge case when calculating GPA excluding instructor & no other instructors are teaching"){
     SemesterClass discreteStructures(2015,"Fall","CS",173);
     REQUIRE(0 == testFrame.getGPAExcludingInstructor(discreteStructures,"Fleck, Margaret M"));
@@ -135,7 +148,10 @@ TEST_CASE("Test GPA excluding instructor is correct"){
     REQUIRE(isWithinTolerance(gpaExcludingCraig,otherInstructorGPA));
 }
 
+
+
 /**
+ * ---------------------------SECTION--------------------------
  * The following tests are extremely important, as not having a strict-ordering
  * relation defined will cause errors in comptuation.
  */
